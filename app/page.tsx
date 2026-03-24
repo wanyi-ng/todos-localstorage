c1bdb9a844b8b67d4e8545c53997e478e7eee3f1
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-23 23:54:55 -0400

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import TodoForm from "@/components/TodoForm"
 
+export const metadata = {
+  title: "Todo App",
+  description: "Add and manage your tasks. Saved to local storage.",
+  keywords: ["todo", "todos", "todo app", "todos app", "tasks", "task"],
+}
+
 export default async function TodoPage() {
   return (
     <main className="flex flex-col h-full min-h-screen p-24">
